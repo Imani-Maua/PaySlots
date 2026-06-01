98bd3c6c588bbf672b18afbb6f02973662394f56
Maua Elaine Imani <elainewalter7022@gmail.com>
2026-06-01 16:00:08 +0200

    feat: added logic for checking plan limits

--- domain/plan.go
+++ domain/plan.go
@@ -1,1 +1,29 @@
 package domain
+
+
+
+type PlanName string
+
+type Plan struct{
+	ID int64
+	Name PlanName
+	PriceUSD int64
+	MaxTalents *int
+	MaxDepartments *int
+	MLEnabled bool
+}
+
+const (
+	FreePlan PlanName = "free"
+	StarterPlan PlanName = "starter"
+	ProPlan PlanName = "pro"
+)
+
+
+func (plan * Plan) WithinLimit(limit *int, count int) bool{
+	if limit == nil{
+		return true
+	}
+
+	return count <= *limit
+}
